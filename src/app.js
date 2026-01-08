206b50d7f9c6d2bcfdde6b7e93125e76adcc9dc0
Custom coder <customcoder245@gmail.com>
2026-01-08 16:25:03 +0530

    app.js change

--- src/app.js
+++ src/app.js
@@ -17,9 +17,7 @@ app.use(cookieParser());
 // Enable CORS
 app.use(
   cors({
-    // origin: "https://talent-by-design.vercel.app/",
-    origin: 'https://talent-by-design.vercel.app',
-    credentials: true
+    origin: 'https://talent-by-design.vercel.app'
   })
 );
 
